11fe47779a85950ec6607ea1b60559eeada36f9a
Kiel Haymon <haymon.kiel@gmail.com>
2023-10-14 20:50:07 -0500

    added footer

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -21,7 +21,7 @@
       <a
       href="https://www.linkedin.com/in/kiel-haymon-02489b80/"
       target="_blank"
-     class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
+     class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
       <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
       />
       <h4 class="relative z-9">Get In Touch &rarr;</h4>
